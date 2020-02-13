dd41a62dcf8e9af0f424598919d511b658fcbc79
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-13 07:14:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,8 +29,8 @@ def initial_round
 end
 
 def hit?(card_total)
-   prompt_user
-  get_user_input
+  prompt_user
+  input = get_user_input
   if get_user_input == "h"
     card_total += deal_card
   end
